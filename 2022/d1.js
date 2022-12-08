452cf802cda991b318efd7f7717d88cfc9702617
Mark Young <mainstreetmark@gmail.com>
2022-12-08 08:26:13 -0500

    d4p2

--- 2022/d1.js
+++ 2022/d1.js
@@ -1,5 +1,9 @@
 import fs from "fs";
 
+function in_range(range, val) {
+	return val >= range[0] && val <= range[1];
+}
+
 fs.readFile("d1.txt", "utf8", (err, contents) => {
 	let count = 0;
 	const lines = contents.split("\n");
@@ -9,12 +13,14 @@ fs.readFile("d1.txt", "utf8", (err, contents) => {
 			// console.log(pairs);
 			const r1 = pairs[0].split("-").map((n) => Number(n));
 			const r2 = pairs[1].split("-").map((n) => Number(n));
-			if (r2[0] >= r1[0] && r2[1] <= r1[1]) {
+			if (in_range(r1, r2[0])) {
 				count++;
-				console.log("r1", pairs);
-			} else if (r1[0] >= r2[0] && r1[1] <= r2[1]) {
+			} else if (in_range(r1, r2[1])) {
 				count++;
-				console.log("r2", pairs);
+			} else if (in_range(r2, r1[0])) {
+				count++;
+			} else if (in_range(r2, r1[1])) {
+				count++;
 			}
 		}
 	}
