710a9147d109529f5e63861abd958c5f3dbfe67f
Mark Young <mainstreetmark@gmail.com>
2022-12-09 08:56:22 -0500

    d5p2

--- 2022/e1.js
+++ 2022/e1.js
@@ -3,38 +3,39 @@ const stacklines = [];
 const movelines = [];
 
 function stack_to_array(stack) {
-	const length = Math.max(...stack.map(s => s.length));
-	const cols = (length+1)/4
-	console.log("len", length,"cols", cols);
+	const length = Math.max(...stack.map((s) => s.length));
+	const cols = (length + 1) / 4;
+	console.log("len", length, "cols", cols);
 	const array = [];
 
-	for (var l =stack.length-1; l>=0; l--) {
-		const r= 0;
-		let row = stack[l].padEnd(length).replace(/[\[\]]/g,' ');
+	for (var l = stack.length - 1; l >= 0; l--) {
+		const r = 0;
+		let row = stack[l].padEnd(length).replace(/[\[\]]/g, " ");
 		// console.log(">", row,"<");
-		let string = '';
-		for(var col = 1; col < length; col += 4) {
-			if(!array[(col-1)/4]) array[(col-1)/4]='';
-			array[(col-1)/4]+=row[col]
+		let string = "";
+		for (var col = 1; col < length; col += 4) {
+			if (!array[(col - 1) / 4]) array[(col - 1) / 4] = "";
+			array[(col - 1) / 4] += row[col];
 		}
 		// array.push(string);
 	}
 
-	return array.map(a => a.trim());
+	return array.map((a) => a.trim());
 }
 
 function Move(stacks, count, from, to) {
 	count = Number(count);
-	from = Number(from)-1;
-	to = Number(to)-1;
-	console.log("move", stacks[1], count, from , to);
-	for (var c = 0; c<count; c++) {
-		var item = stacks[from].split('').pop();
-		stacks[from] = stacks[from].replace(/.$/, '')
-		stacks[to]+=item;
-		// console.log("  >", item);
-	}
-	return stacks
+	from = Number(from) - 1;
+	to = Number(to) - 1;
+	console.log("move", stacks[1], count, from, to);
+
+	var item = stacks[from].substring(stacks[from].length - count);
+	console.log(count, item);
+	stacks[from] = stacks[from].substring(0, stacks[from].length - count);
+	stacks[to] += item;
+	// console.log("  >", item);
+
+	return stacks;
 }
 
 fs.readFile("e1.txt", "utf8", (err, contents) => {
@@ -46,13 +47,12 @@ fs.readFile("e1.txt", "utf8", (err, contents) => {
 	console.log();
 
 	let stacks = stack_to_array(stacklines);
+	console.log(stacks);
 	for (var m of movelines) {
-		const [ x, count,y , from,z , to ] = m.split(" ");
-		stacks = Move(stacks, count, from,to);
+		const [x, count, y, from, z, to] = m.split(" ");
+		stacks = Move(stacks, count, from, to);
 		console.log(stacks);
 	}
 
-	console.log("out>", stacks.map(s => s.split('').pop()).join(''))
-
+	console.log("out>", stacks.map((s) => s.split("").pop()).join(""));
 });
-
