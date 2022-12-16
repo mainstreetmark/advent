0ce6bf61de55ba26093249b5b045496f5b4f8ab4
Mark Young <mainstreetmark@gmail.com>
2022-12-16 07:56:19 -0500

    d10p1

---
+++ 2022/d10.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+
+let cycle = 0;
+let X = 1;
+let cost = 0;
+let dx = 0;
+let sum = 0;
+fs.readFile("d10s.txt", "utf8", (err, contents) => {
+	const lines = contents.split("\n");
+	for (var l of lines) {
+		const [cmd, arg] = l.split(" ");
+		switch (cmd) {
+			case "addx":
+				dx = Number(arg);
+				cost = 2;
+				break;
+			case "noop":
+				cost = 1;
+				break;
+		}
+		for (var c = 0; c < cost; c++) {
+			cycle++;
+			console.log(cycle, l, ":", X);
+			if (cycle == 20 || (cycle + 20) % 40 == 0) sum += cycle * X;
+		}
+		X += dx;
+		if (dx) console.log("  -> X=", X);
+		dx = 0;
+	}
+	console.log(">>", sum);
+});
