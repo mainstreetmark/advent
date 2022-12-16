9ebffb019c98e4ac1292a24297864dfdd2b1c9b2
Mark Young <mainstreetmark@gmail.com>
2022-12-16 08:11:30 -0500

    d10p2

--- 2022/d10.js
+++ 2022/d10.js
@@ -5,6 +5,9 @@ let X = 1;
 let cost = 0;
 let dx = 0;
 let sum = 0;
+let col = 0;
+
+let line = "";
 fs.readFile("d10.txt", "utf8", (err, contents) => {
 	const lines = contents.split("\n");
 	for (var l of lines) {
@@ -20,11 +23,20 @@ fs.readFile("d10.txt", "utf8", (err, contents) => {
 		}
 		for (var c = 0; c < cost; c++) {
 			cycle++;
-			console.log(cycle, l, ":", X);
-			if (cycle == 20 || (cycle + 20) % 40 == 0) sum += cycle * X;
+			// console.log(cycle, l, ":", X);
+			let px = ".";
+			if (X == col - 1 || X == col || X == col + 1) px = "#";
+			line += px;
+			col++;
+			if (col == 40) {
+				console.log(line);
+				col = 0;
+				line = "";
+			}
+			// if (cycle == 20 || (cycle + 20) % 40 == 0) sum += cycle * X;
 		}
 		X += dx;
-		if (dx) console.log("  -> X=", X);
+		// if (dx) console.log("  -> X=", X);
 		dx = 0;
 	}
 	console.log(">>", sum);
