b5624fc8cc9d03bd54cf94de8dcce3e8150449c9
Mark Young <mainstreetmark@gmail.com>
2023-12-12 07:39:09 -0500

    day6 done

--- 2023/f1.js
+++ 2023/f1.js
@@ -1,10 +1,30 @@
 import fs from "fs";
 
+console.log(process.argv[2]);
+
 fs.readFile(process.argv[2], "utf8", (err, contents) => {
 	const lines = contents.trim().split("\n");
-	for (var l of lines) {
-		if (l) {
-			console.log(l);
+	// split on whitespace
+	let times = lines[0].split(/\s+/).slice(1).join("");
+	let distances = lines[1].split(/\s+/).slice(1).join("");
+	times = [times].map(Number);
+	distances = [distances].map(Number);
+	console.log(times, distances);
+
+	let wins = [];
+	for (var r = 0; r < times.length; r++) {
+		// console.log(times[r]);
+		wins[r] = 0;
+		for (var T = 0; T < times[r]; T++) {
+			let dist = T * (times[r] - T);
+			if (dist > distances[r]) wins[r]++;
+			// console.log(T, dist);
 		}
+		// break;
 	}
+	console.log(wins);
+	console.log(
+		">>",
+		wins.reduce((a, b) => a * b, 1)
+	);
 });
