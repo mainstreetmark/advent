8f924b3f3efce8c2d9b2537c4dcb79e7355a6bb4
Mark Young <mainstreetmark@gmail.com>
2023-12-13 07:19:02 -0500

    day 8 finish

--- 2023/h1.js
+++ 2023/h1.js
@@ -1,6 +1,28 @@
 import fs from "fs";
+import lcm from "compute-lcm";
 
 const MAP = {};
+
+// function lcm(arr) {
+// 	// Kind of a brute force method, It's not fancy but it's very simple and easy to read :P
+
+// 	// make an array with all the numbers in the range.
+// 	let numbersArr = [...arr];
+// 	// for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+// 	// 	numbersArr.push(i);
+// 	// }
+
+// 	// keep multiplying the biggest number until it's divisible by all the numbers in the numbersArr array.
+// 	let scm = Math.max(...arr);
+// 	while (true) {
+// 		if (numbersArr.every((num) => scm % num === 0)) {
+// 			return scm;
+// 		} else {
+// 			scm += Math.max(...arr);
+// 			5;
+// 		}
+// 	}
+// }
 
 fs.readFile(process.argv[2], "utf8", (err, contents) => {
 	const lines = contents.trim().split("\n");
@@ -15,29 +37,53 @@ fs.readFile(process.argv[2], "utf8", (err, contents) => {
 		}
 	}
 	const dirlength = dirs.length;
-	const starts = Object.keys(MAP).filter((k) => k[2] == "A");
+	let starts = Object.keys(MAP).filter((k) => k[2] == "A");
+	// starts = [starts[0]];
 	console.log(starts);
 
-	const keys = starts.map((k) => ({ key: k }));
+	const keys = starts.map((k) => ({
+		key: k,
+		path: [],
+		start: k,
+		loop: 0,
+	}));
 	let step = 0;
 	let begin = Date.now();
 	let loop = true;
 	const len = keys.length;
 	while (loop) {
-		// console.log(keys.filter((k) => k.key[2] == "Z").length);
 		const index = step % dirlength;
-		loop = false;
+		// loop = false^;
 		for (var i = 0; i < len; i++) {
 			const k = keys[i];
+			k.path.push(k.key);
+			// if (!k.start) k.start = k.key;
 			k.key = MAP[k.key][dirs[index]];
-			loop = loop || k.key[2] !== "Z";
+			if (k.key[2] == "Z") {
+				if (k.loop == 0) {
+					console.log(k.key);
+					k.loop = k.path.length;
+				}
+			}
+		}
+		console.log(keys.filter((k) => k.loop == 0).length);
+		if (keys.filter((k) => k.loop == 0).length == 0) {
+			break;
 		}
 		step++;
+		// loop = step < 1000;
 		// console.log("step", step, loop);
 		if (step % 100000000 == 0) {
 			console.log(step.toLocaleString(), Date.now() - begin);
 			begin = Date.now();
 		}
 	}
-	console.log(">>", step);
+	console.log(
+		"end",
+		keys,
+		keys.map((k) => k.loop)
+	);
+	console.log(lcm(keys.map((k) => k.loop)));
+	debugger;
+	// console.log(">>", step);
 });
