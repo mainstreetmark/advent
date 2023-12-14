f580f965f19f6f80c485de1d28a54a30dfa26afd
Mark Young <mainstreetmark@gmail.com>
2023-12-14 13:11:14 -0500

    d11 done

--- 2023/d11.js
+++ 2023/d11.js
@@ -1,19 +1,21 @@
 import fs from "fs";
 const MAP = [];
 const COORD = [];
-const INFLATE = 2;
+const INFLATE = 1000000;
+let DIM = { r: 0, c: 0 };
 fs.readFile(process.argv[2], "utf8", (err, contents) => {
 	const lines = contents.trim().split("\n");
-	for (var l of lines) {
-		if (l) {
-			MAP.push(l);
-			if (!l.includes("#"))
-				for (var i = 0; i < INFLATE - 1; i++) MAP.push(l);
+	for (var r = 0; r < lines.length; r++) {
+		for (var c = 0; c < lines[r].length; c++) {
+			if (lines[r][c] === "#") {
+				COORD.push({ c, r, min: Infinity, to: [] });
+			}
 		}
 	}
+	DIM = { r: lines.length, c: lines[0].length };
 	Expand();
-	// console.log(MAP.join("\n"), MAP.length);
-	Identify();
+	// // console.log(MAP.join("\n"), MAP.length);
+	// Identify();
 	Path();
 	// console.log(COORD);
 	debugger;
@@ -53,29 +55,64 @@ function Identify() {
 }
 
 function Expand() {
-	const cols = [];
-	for (var c = 0; c < MAP[0].length; c++) {
-		let found = false;
-		for (var row of MAP) {
-			if (row[c] === "#") {
-				found = true;
-				break;
-			}
-		}
-		if (!found) {
-			cols.push(c);
+	var matchc = Array.from(new Set([...COORD.map((c) => c.c).sort()]));
+	var cols = [];
+	for (var c = 0; c < DIM.c; c++) if (!matchc.includes(c)) cols.push(c);
+
+	// pre-expand the colums and rows
+	for (var c = 0; c < cols.length; c++)
+		for (var c1 = c + 1; c1 < cols.length; c1++) cols[c1] += INFLATE - 1;
+
+	var matchr = Array.from(new Set([...COORD.map((c) => c.r).sort()]));
+	var rows = [];
+	for (var c = 0; c < DIM.r; c++) if (!matchr.includes(c)) rows.push(c);
+	console.log("a", rows);
+	for (var c = 0; c < rows.length; c++)
+		for (var c1 = c + 1; c1 < rows.length; c1++) rows[c1] += INFLATE - 1;
+	console.log("b", rows);
+
+	console.log(COORD);
+
+	for (var r of rows) {
+		console.log(r);
+		for (var C = 0; C < COORD.length; C++) {
+			if (COORD[C].r > r) COORD[C].r += INFLATE - 1;
 		}
 	}
-	for (var r = 0; r < MAP.length; r++) {
-		let row = MAP[r];
-		let row2 = "";
-		console.log(r, MAP.length);
-		for (var c = 0; c < row.length; c++) {
-			row2 += row[c];
-			if (cols.includes(c)) {
-				for (var i = 0; i < INFLATE - 1; i++) row2 += row[c];
-			}
+
+	for (var c of cols) {
+		for (var C = 0; C < COORD.length; C++) {
+			if (COORD[C].c > c) COORD[C].c += INFLATE - 1;
 		}
-		MAP[r] = row2;
 	}
+
+	console.log(COORD);
 }
+
+// function Expand() {
+// 	const cols = [];
+// 	for (var c = 0; c < MAP[0].length; c++) {
+// 		let found = false;
+// 		for (var row of MAP) {
+// 			if (row[c] === "#") {
+// 				found = true;
+// 				break;
+// 			}
+// 		}
+// 		if (!found) {
+// 			cols.push(c);
+// 		}
+// 	}
+// 	for (var r = 0; r < MAP.length; r++) {
+// 		let row = MAP[r];
+// 		let row2 = "";
+// 		console.log(r, MAP.length);
+// 		for (var c = 0; c < row.length; c++) {
+// 			row2 += row[c];
+// 			if (cols.includes(c)) {
+// 				for (var i = 0; i < INFLATE - 1; i++) row2 += row[c];
+// 			}
+// 		}
+// 		MAP[r] = row2;
+// 	}
+// }
