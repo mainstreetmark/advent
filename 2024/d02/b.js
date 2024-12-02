c3f849118ad5fb76c6867168ed06d1868cdb90fe
Mark Young <mainstreetmark@gmail.com>
2024-12-02 15:02:29 -0500

    d2p2

--- 2024/d02/b.js
+++ 2024/d02/b.js
@@ -17,7 +17,7 @@ function count_fails(report) {
 }
 
 function check_safe(report, dir = null) {
-	// console.log("< ", attempt, report);
+	// console.log("> ", report);
 	var passes = 1;
 	for (var i = 1; i < report.length; i++) {
 		if (!dir) {
@@ -25,20 +25,37 @@ function check_safe(report, dir = null) {
 			else dir = [1, 3];
 		}
 		let diff = report[i] - report[i - 1];
-		if (diff < dir[0] || diff > dir[1]) {
-			// console.log("diff", diff, report[i - 1], report[i]);
-			if (passes > 0) {
-				diff = report[i + 1] - report[i - 1];
-				if (isNaN(report[i + 1])) return 1;
-				if (isNaN(diff)) debugger;
-				// console.log("diff retrt", diff, report[i - 1], report[i + 1]);
-				if (diff < dir[0] || diff > dir[1]) return 0;
-				report = remove_index(report, i);
-				passes--;
-			} else return 0;
-		}
+		if (diff < dir[0] || diff > dir[1]) return 0;
 	}
+
 	return 1;
+}
+
+function get_deltas(report) {
+	const delta = [];
+	for (var i = 1; i < report.length; i++) {
+		delta.push(report[i] - report[i - 1]);
+	}
+	return delta;
+}
+
+function check_unsafe(report) {
+	var dir = null;
+	console.log("\n >>unsafe:", report.join(" "));
+	let pass = 0;
+	for (var i = 0; i < report.length; i++) {
+		var report2 = remove_index(report, i);
+		console.log(
+			"    report2: ",
+			report2.join(" "),
+			"=",
+			check_safe(report2)
+		);
+		pass += check_safe(report2);
+	}
+	if (pass > 0) console.log("    passes: ", pass);
+	// console.log("    passes: ", pass);
+	return pass > 0;
 }
 
 const __dirname = fileURLToPath(dirname(import.meta.url));
@@ -51,9 +68,16 @@ fs.readFile(datafile, "utf8", (err, contents) => {
 		.filter((line) => line?.length > 0);
 	let good = 0;
 	for (var report of split_lines) {
+		// console.log(get_deltas(report));
 		var result = check_safe(report);
-		if (!result)
-			console.log("result", report.join(","), result ? "Safe" : "Unsafe");
+		if (!result) result = check_unsafe(report);
+		// if (!result)
+		console.log(
+			"result",
+			report.join(","),
+			get_deltas(report),
+			result ? "Safe" : "Unsafe"
+		);
 		good += result;
 	}
 	console.log(">>", good);
