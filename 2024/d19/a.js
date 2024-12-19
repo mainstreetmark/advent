9fb29da86a1017e8c98799835443148d51f46623
Mark Young <mainstreetmark@gmail.com>
2024-12-19 07:07:56 -0500

    d19p1

--- 2024/d19/a.js
+++ 2024/d19/a.js
@@ -2,6 +2,8 @@ import fs from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
+var CACHE = {};
+
 const __dirname = fileURLToPath(dirname(import.meta.url));
 var datafile = __dirname + "/d19.txt";
 console.log("\n---\n", datafile);
@@ -9,20 +11,31 @@ fs.readFile(datafile, "utf8", (err, contents) => {
 	Go(contents);
 });
 
-function Validate(design, patterns) {
-	// console.log("   Validate", design);
+function Validate(design, patterns, depth = 0) {
 	if (design == "") return 1;
-	const patternsf = patterns.filter((p) => design.length >= p.length);
+	if (design.length > 0 && patterns.length === 0) return 0;
+
+	const patternsf = patterns.filter((p) => design.indexOf(p) == 0);
+	var count = 0;
 	for (var pattern of patternsf) {
-		var part = design.slice(0, pattern.length);
-		if (part == pattern) {
-			// console.log("     match", part);
-			if (Validate(design.slice(pattern.length), patterns)) return 1;
+		// console.log(count++, " ".repeat(depth), design, pattern, patternsf);
+		const key = `${design}|${pattern}`;
+		if (CACHE[key] === undefined) {
+			CACHE[key] = Validate(
+				design.slice(pattern.length),
+				patterns,
+				depth + 1
+			);
+		}
+		if (CACHE[key]) {
+			return 1;
 		}
 	}
-
 	return 0;
 }
+
+// 236 low
+// 401 high
 
 function Go(contents) {
 	const lines = contents.trim().split("\n");
@@ -34,6 +47,7 @@ function Go(contents) {
 	lines.shift();
 	var possible = 0;
 	for (var design of lines) {
+		// console.log("\n DESSIGN:", design);
 		if (Validate(design, patterns)) {
 			// console.log("  VALID", design, possible);
 			possible++;
